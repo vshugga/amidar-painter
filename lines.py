bb6f46e8c66ce7ff921207d8ddd8a65ed4580858
vshugga <v@vshugga.net>
2026-07-02 01:34:16 -0400

    movement restriction to grid

--- lines.py
+++ lines.py
@@ -13,8 +13,6 @@ line_thickness = 8.0
 rect_origin = Vector2((w/2)-(rect_x/2),(h/2)-(rect_y/2))
 # rect = Rectangle(rect_origin.x, rect_origin.y, rect_x, rect_y) # perimeter
 
-
-
 v_lines_num = 6
 h_lines_minmax = (4,6) # number of horizontal lines in a column
 h_lines_range = (rect_y // 10, rect_y // 3) # min/max height of a cell
@@ -22,11 +20,12 @@ h_lines_range = (rect_y // 10, rect_y // 3) # min/max height of a cell
 cell_width = rect_x/(v_lines_num-1)
 # pairs of vectors for inner lines top/bottom point
 v_positions = [(Vector2(rect_origin.x+cell_width*i,rect_origin.y), Vector2(rect_origin.x+cell_width*i,rect_origin.y+rect_y)) for i in range(v_lines_num)]
+
 # pairs of vectors for inner horizontal lines left/right point
 h_positions = [] 
 
 #TODO: This algorithm needs rewritten to avoid horizontal lines that are directly adjacent
-# my idea is to have a 'nudge' conditional that checks if a line will be too close, and nudges 10 pixels if so
+# probably have the h_lines thing place them as it goes, placing the earliest it can (have only the first column be random)
 
 for vl in range(v_lines_num-1): # -1 to prevent H lines on last v line
     # x = rect_origin.x+(vl*cell_width)
@@ -61,14 +60,20 @@ class Player():
         self.direction = Vector2()
         self.speed = 100
 
-    def move_x(self, x):
-        if not (rect_origin.x < x < (rect_x+rect_origin.x)):
-            return
-        
-        self.pos.x = x
+    def move_x(self, x, y):
+        for hl in h_positions:
+            if -1 < hl[0].y - y < 1 and hl[0].x <= x <= hl[1].x:
+                self.pos.x = x
+                return
 
-    def move_y(self, y):
-        self.pos.y = y
+    def move_y(self, x, y):
+        for vl in v_positions:
+            if -1 < vl[0].x - x < 1 and vl[0].y <= y <= vl[1].y:
+                self.pos.y = y
+                return
+
+
+
 
     def update(self):
         self.direction.x = int(is_key_down(rl.KEY_RIGHT)) - int(is_key_down(rl.KEY_LEFT))
@@ -78,8 +83,11 @@ class Player():
         new_x = self.pos.x + (self.direction.x * self.speed * dt)
         new_y = self.pos.y + (self.direction.y * self.speed * dt)
 
-        self.move_x(new_x)
-        self.move_y(new_y)
+
+        if new_x != self.pos.x:
+            self.move_x(new_x, new_y)
+        if new_y != self.pos.y:
+            self.move_y(new_x, new_y)
 
 
     def draw(self):
