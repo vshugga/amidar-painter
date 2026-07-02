bdd7175c639d22b5b7622c70301caf85e06558c3
vshugga <v@vshugga.net>
2026-07-02 01:34:16 -0400

    draw perimeter with lines instead

--- lines.py
+++ lines.py
@@ -2,24 +2,6 @@ from pyray import *
 import raylib as rl
 from random import randrange
 
-class Player():
-    def __init__(self, x, y, w, h, line_thickness) -> None:
-        self.pos = Vector2(x, y)
-        self.size = Vector2(w, h)
-        self.line_thickness = line_thickness
-        self.direction = Vector2()
-
-    def update(self):
-        self.direction.x = int(is_key_down(rl.KEY_RIGHT)) - int(is_key_down(rl.KEY_LEFT))
-        self.direction.y = int(is_key_down(rl.KEY_DOWN)) - int(is_key_down(rl.KEY_UP))
-
-    def draw(self):
-        x_offset = (self.size.x / -2) + (line_thickness / 2)
-        y_offset = (self.size.y / -2) - (line_thickness / 2)
-        p = Vector2(self.pos.x + x_offset, self.pos.y + y_offset)
-        draw_rectangle_v(p, self.size, GREEN)
-
-
 w, h = 1080, 720
 
 init_window(w, h, 'amidar')
@@ -29,23 +11,26 @@ set_exit_key(rl.KEY_ESCAPE)
 rect_x, rect_y = 650, 650
 line_thickness = 8.0
 rect_origin = Vector2((w/2)-(rect_x/2),(h/2)-(rect_y/2))
-rect = Rectangle(rect_origin.x, rect_origin.y, rect_x, rect_y)
-player = Player(rect_origin.x, rect_origin.y+rect_y, 20, 20, line_thickness)
+# rect = Rectangle(rect_origin.x, rect_origin.y, rect_x, rect_y) # perimeter
 
 
-v_lines_num = 4 # number of inner vertical lines, excludes perimeter
+
+v_lines_num = 6
 h_lines_minmax = (4,6) # number of horizontal lines in a column
 h_lines_range = (rect_y // 10, rect_y // 3) # min/max height of a cell
 
-cell_width = rect_x/(v_lines_num+1)
-v_positions = [(Vector2(rect_origin.x+cell_width*i,rect_origin.y), Vector2(rect_origin.x+cell_width*i,rect_origin.y+rect_y)) for i in range(1, v_lines_num+1)]
-h_positions = []
+cell_width = rect_x/(v_lines_num-1)
+# pairs of vectors for inner lines top/bottom point
+v_positions = [(Vector2(rect_origin.x+cell_width*i,rect_origin.y), Vector2(rect_origin.x+cell_width*i,rect_origin.y+rect_y)) for i in range(v_lines_num)]
+# pairs of vectors for inner horizontal lines left/right point
+h_positions = [] 
 
 #TODO: This algorithm needs rewritten to avoid horizontal lines that are directly adjacent
 # my idea is to have a 'nudge' conditional that checks if a line will be too close, and nudges 10 pixels if so
 
-for vl in range(v_lines_num+1):
-    x = rect_origin.x+(vl*cell_width)
+for vl in range(v_lines_num-1): # -1 to prevent H lines on last v line
+    # x = rect_origin.x+(vl*cell_width)
+    x = v_positions[vl][0].x
     y = rect_origin.y
     h_lines = randrange(*h_lines_minmax)
     for hl in range(h_lines):
@@ -61,17 +46,62 @@ for vl in range(v_lines_num+1):
         nl1 = Vector2(x, y)
         nl2 = Vector2(x+cell_width,y)
         h_positions += [(nl1, nl2)]
-            
+
+#TODO: fix the corner edges           
+top_h_line = [(Vector2(rect_origin.x, rect_origin.y), Vector2(rect_origin.x+rect_x, rect_origin.y))]
+bottom_h_line = [(Vector2(rect_origin.x, rect_origin.y+rect_y), Vector2(rect_origin.x+rect_x, rect_origin.y+rect_y))]
+
+h_positions += top_h_line + bottom_h_line
+
+class Player():
+    def __init__(self, x, y, w, h) -> None:
+        self.pos = Vector2(x, y)
+        self.size = Vector2(w, h)
+        # self.line_thickness = line_thickness
+        self.direction = Vector2()
+        self.speed = 100
+
+    def move_x(self, x):
+        if not (rect_origin.x < x < (rect_x+rect_origin.x)):
+            return
+        
+        self.pos.x = x
+
+    def move_y(self, y):
+        self.pos.y = y
+
+    def update(self):
+        self.direction.x = int(is_key_down(rl.KEY_RIGHT)) - int(is_key_down(rl.KEY_LEFT))
+        self.direction.y = int(is_key_down(rl.KEY_DOWN)) - int(is_key_down(rl.KEY_UP))
+
+        dt = get_frame_time()
+        new_x = self.pos.x + (self.direction.x * self.speed * dt)
+        new_y = self.pos.y + (self.direction.y * self.speed * dt)
+
+        self.move_x(new_x)
+        self.move_y(new_y)
+
+
+    def draw(self):
+        x_offset = 0#(self.size.x / -2) + (line_thickness / 2)
+        y_offset = 0#(self.size.y / -2) - (line_thickness / 2) 
+        p = Vector2(self.pos.x + x_offset, self.pos.y + y_offset)
+        draw_rectangle_v(p, self.size, GREEN)
+
+
+
+player = Player(rect_origin.x, rect_origin.y+rect_y, 20, 20)
+
 
 
 
 while not window_should_close():
-    # player.update()
+    player.update()
 
     begin_drawing()
 
     # Draw the grid thing
-    draw_rectangle_lines_ex(rect, line_thickness, RED)
+    # draw_rectangle_lines_ex(rect, line_thickness, RED)
     for l in v_positions:
         draw_line_ex(*l,line_thickness, RED)
     for h in h_positions:
