d0230e09fcc8d5fc0e924b499cead741c8eea473
vshugga <v@vshugga.net>
2026-07-02 01:34:16 -0400

    placeholder player

--- lines.py
+++ lines.py
@@ -2,17 +2,36 @@ from pyray import *
 import raylib as rl
 from random import randrange
 
+class Player():
+    def __init__(self, x, y, w, h, line_thickness) -> None:
+        self.pos = Vector2(x, y)
+        self.size = Vector2(w, h)
+        self.line_thickness = line_thickness
+        self.direction = Vector2()
+
+    def update(self):
+        self.direction.x = int(is_key_down(rl.KEY_RIGHT)) - int(is_key_down(rl.KEY_LEFT))
+        self.direction.y = int(is_key_down(rl.KEY_DOWN)) - int(is_key_down(rl.KEY_UP))
+
+    def draw(self):
+        x_offset = (self.size.x / -2) + (line_thickness / 2)
+        y_offset = (self.size.y / -2) - (line_thickness / 2)
+        p = Vector2(self.pos.x + x_offset, self.pos.y + y_offset)
+        draw_rectangle_v(p, self.size, GREEN)
+
 
 w, h = 1080, 720
-
 
 init_window(w, h, 'amidar')
 set_exit_key(rl.KEY_ESCAPE)
 
-rect_x, rect_y = 600, 600
+
+rect_x, rect_y = 650, 650
 line_thickness = 8.0
 rect_origin = Vector2((w/2)-(rect_x/2),(h/2)-(rect_y/2))
 rect = Rectangle(rect_origin.x, rect_origin.y, rect_x, rect_y)
+player = Player(rect_origin.x, rect_origin.y+rect_y, 20, 20, line_thickness)
+
 
 v_lines_num = 4 # number of inner vertical lines, excludes perimeter
 h_lines_minmax = (4,6) # number of horizontal lines in a column
@@ -22,7 +41,8 @@ cell_width = rect_x/(v_lines_num+1)
 v_positions = [(Vector2(rect_origin.x+cell_width*i,rect_origin.y), Vector2(rect_origin.x+cell_width*i,rect_origin.y+rect_y)) for i in range(1, v_lines_num+1)]
 h_positions = []
 
-#TODO: This algorithm needs rewritten to avoid horizontal lines that are directly adjacent 
+#TODO: This algorithm needs rewritten to avoid horizontal lines that are directly adjacent
+# my idea is to have a 'nudge' conditional that checks if a line will be too close, and nudges 10 pixels if so
 
 for vl in range(v_lines_num+1):
     x = rect_origin.x+(vl*cell_width)
@@ -35,7 +55,7 @@ for vl in range(v_lines_num+1):
             remaining = rect_origin.y+rect_y-ly
             if remaining > h_lines_range[1]:
                 y = (remaining//2) + ly # if the remaining cell size would be larger than allowed, split it in half
-                print('half on col ' + str(vl))
+                # print('half on col ' + str(vl))
             else:
                 break
         nl1 = Vector2(x, y)
@@ -43,14 +63,22 @@ for vl in range(v_lines_num+1):
         h_positions += [(nl1, nl2)]
             
 
+
+
 while not window_should_close():
+    # player.update()
+
     begin_drawing()
+
+    # Draw the grid thing
     draw_rectangle_lines_ex(rect, line_thickness, RED)
     for l in v_positions:
         draw_line_ex(*l,line_thickness, RED)
-
     for h in h_positions:
         draw_line_ex(*h, line_thickness, RED)
+
+    #draw player
+    player.draw()
 
     clear_background(BLACK)
     end_drawing()
